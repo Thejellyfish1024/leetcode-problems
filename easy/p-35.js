789ca3cd9bd5dd3817c4e83952d4c405f9db2b40
Thejellyfish1024 <alamin102410@gmail.com>
2024-07-02 14:18:56 +0600

    more

---
+++ easy/p-35.js
@@ -0,0 +1,30 @@
+// 35. Search Insert Position
+
+// Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+// Example 1:
+// Input: nums = [1,3,5,6], target = 5
+// Output: 2
+
+// Example 2:
+// Input: nums = [1,3,5,6], target = 2
+// Output: 1
+
+// Example 3:
+// Input: nums = [1,3,5,6], target = 7
+// Output: 4
+
+
+var searchInsert = function (nums, target) {
+    if (nums.includes(target)) return nums.indexOf(target);
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] > target) return 0;
+        if (i === nums.length - 1) return nums.length;
+        if (nums[i] < target && nums[i + 1] > target) return i + 1;
+    }
+};
+
+console.log(searchInsert([1, 3, 5, 6], 0));
+
